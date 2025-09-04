360cbf124847e0ae8d8bd6d95612f11cfa51e5c3
up33anas <anassabir1072@gmail.com>
2025-09-04 21:11:35 +0500

    Rename the folder

--- server using node/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const { logEvents } = require("./logEvents");
-
-const errorHandler = (error, req, res, next) => {
-  logEvents(`${error.name}: ${error.message}`, "errorLog.txt");
-  console.error(error.stack);
-  res.status(500).send(error.message);
-};
-
-module.exports = errorHandler;
--- server using node/routes/refresh.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const refreshController = require("../controllers/refreshTokenController");
-
-router.get("/", refreshController.handleRefreshToken);
-
-module.exports = router;
